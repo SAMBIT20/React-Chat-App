6aced7dabffbda8585970739a1603f1662a030f6
Sambit Kumar Nanda <sambitn20@gmail.com>
2021-12-13 14:03:02 +0530

    first commit

---
+++ src/components/userProfile/UserProfile.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./userProfile.css";
+
+export default class UserProfile extends Component {
+  toggleInfo = (e) => {
+    e.target.parentNode.classList.toggle("open");
+  };
+  render() {
+    return (
+      <div className="main__userprofile">
+        <div className="profile__card user__profile__image">
+          <div className="profile__image">
+            <img src="https://files.codingninjas.com/profile173053f4ad7ce4e39fba39619822a22c2da3d2.jpeg" />
+          </div>
+          <h4>Fernando Faucho</h4>
+          <p>CEO & Founder at Highly Inc</p>
+        </div>
+        <div className="profile__card">
+          <div className="card__header" onClick={this.toggleInfo}>
+            <h4>Information</h4>
+            <i className="fa fa-angle-down"></i>
+          </div>
+          <div className="card__content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            ultrices urna a imperdiet egestas. Donec in magna quis ligula
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
